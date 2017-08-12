9f333178fddd98c32641e1529ff21f4bbed39b88
TimothyJohnK <Tim@VectorLab.com>
2017-08-11 22:52:44 -0500

    styled header with correct curry color and flexed it up

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,17 +3,35 @@ import '../App.css';
 import styled from 'styled-components';
 
 const HeaderWrap = styled.div`
-  background-color: yellow;
-  font-type:  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+  background-color: rgba(200, 180, 20, .9);
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
 `;
 
-const Title = styled.h1`
+const Logo = styled.img`
+  flex: 1;
+`
 
+const Title = styled.h1`
+  flex: 6;
+  text-align: center;
 `;
 
 const Phone = styled.h3`
+flex: 1;
   > a {
+    color: black;
     text-decoration: none;
+  };
+  a:hover {
+    color: lightblue;
   }
 `;
 
@@ -21,8 +39,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <HeaderWrap>
-      <Title>Spicehaus Internationale</Title>
-      <Phone><a href='555-555-1234'>(555) 555-1234</a></Phone>
+        <Logo />
+        <Title>Spicehaus Internationale</Title>
+        <Phone><a href='555-555-1234'>(555) 555-1234</a></Phone>
       </HeaderWrap>
     )
   }
